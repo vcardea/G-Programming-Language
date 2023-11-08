70508be30bcb2bcd8950a230a47e580a1a8416b6
vcardea <129986281+vcardea@users.noreply.github.com>
2023-11-08 13:45:18 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file    main.cpp - Main C++ source file G-PL compiler
+ * @author  Vincenzo Cardea (vincenzo.cardea.05@gmail.com)
+ * @version 0.1
+ * @date    2023-11-08
+ *
+ * @copyright Copyright (c) 2023
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
